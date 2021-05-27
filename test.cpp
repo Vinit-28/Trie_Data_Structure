47f2e8509c7de6d27a34d092db13d90c21999829
Vinit-28 <codervinit@gmail.com>
2021-05-27 10:59:35 +0530

    Showing Results of insertion of Repeated Elements and Paths/Routes of Data Elements

--- test.cpp
+++ test.cpp
@@ -10,15 +10,18 @@ int main(){
     Trie<char> trie;
     
     
-    trie.insert( (char*)"abcd", 4 );
-    trie.insert( (char*)"abc", 3 );
-    trie.insert( (char*)"ab", 2 );
-    trie.insert( (char*)"abcde", 5 );
+    
+    cout<<"\nResult of insertion of abcd ( successfull->1 || unsuccessfull->0 ) :: "<<trie.insert( (char*)"abcd", 4 );
+    cout<<"\nResult of insertion of abc ( successfull->1 || unsuccessfull->0 ) :: "<<trie.insert( (char*)"abc", 3 );
+    cout<<"\nResult of insertion of ab ( successfull->1 || unsuccessfull->0 ) :: "<<trie.insert( (char*)"ab", 2 );
+    cout<<"\nResult of insertion of abcde ( successfull->1 || unsuccessfull->0 ) :: "<<trie.insert( (char*)"abcde", 5 );
+
+    cout<<"\nResult of insertion of Reapeated Element abcde ( successfull->1 || unsuccessfull->0 ) :: "<<trie.insert((char*)"abcde",5);
 
 
     vector<vector<char>> routes = trie.get_paths();
 
-    cout<<"\nSequence of Nodes in the Trie ::\n\n";
+    cout<<"\n\nSequence of Nodes in the Trie ::\n\n";
 
     for(int i=0; i<routes.size(); i++){
 
